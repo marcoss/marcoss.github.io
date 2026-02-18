f36ca35ef49f7f9f129f92517186ff2a60276eb2
Marcos <dev@marcos.me>
2026-02-18 10:54:21 -0500

    change chat title

--- src/components/ChatSection.tsx
+++ src/components/ChatSection.tsx
@@ -235,7 +235,7 @@ export function ChatSection() {
 
   return (
     <section>
-      <h2 class="text-xl sm:text-2xl font-semibold mb-5">⚡️ Ask Marcos's AI</h2>
+      <h2 class="text-xl sm:text-2xl font-semibold mb-5">💬 Ask Marcos</h2>
 
       <div
         ref={messagesRef}
